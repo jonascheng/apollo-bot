b71703e6fa9a85855c5c3ce4885a851fbe8a6413
Jonas Cheng <jonas.cheng@gmail.com>
2022-07-06 09:17:09 +0800

    feat: browse to apollo

--- selenium-with-headless-chrome.py
+++ selenium-with-headless-chrome.py
@@ -5,12 +5,11 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-#chrome_options.binary_location=r'/opt/chrome/chromedriver'
 
 driver = webdriver.Chrome(r'/opt/chrome/chromedriver', chrome_options=chrome_options)
-#driver = webdriver.Chrome(executable_path='/opt/chrome/chromedriver', chrome_options=chrome_options)
 
-driver.get("http://python.org")
+driver.get("https://asiaauth.mayohr.com/HRM/Account/Login")
 print("page title: %s" % driver.title)
 
+driver.fullscreen_window()
 driver.save_screenshot('screenshot.png')
