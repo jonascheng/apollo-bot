381d9449b4374a2eb115a73c3cfe788ea989539b
Jonas Cheng <jonas.cheng@gmail.com>
2022-07-06 11:38:09 +0800

    feat: complete feature

--- selenium-with-headless-chrome.py
+++ selenium-with-headless-chrome.py
@@ -19,9 +19,10 @@ driver = webdriver.Chrome(r'/opt/chrome/chromedriver', chrome_options=chrome_opt
 
 driver.fullscreen_window()
 driver.get("https://asiaauth.mayohr.com/HRM/Account/Login")
-print("page title: %s" % driver.title)
 
 def Login():
+    print("%s | Login" % driver.title)
+
     # wait for page loading
     try:
         WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.CLASS_NAME, 'loginform')))
@@ -50,7 +51,44 @@ def Login():
     # submit form
     form.submit()
 
+def WantClockInOut1stLevel():
+    print("%s | WantClockInOut1stLevel" % driver.title)
+
+    # wait for page loading
+    try:
+        WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.CLASS_NAME, 'link-item__title')))
+    except TimeoutException:
+        print("Timed out waiting for page to load")
+
+    # link items
+    link_item = driver.find_element(By.LINK_TEXT, '我要打卡')
+    link_item.click()
+
+def WantClockInOut2ndLevel():
+    print("%s | WantClockInOut2ndLevel" % driver.title)
+
+    # wait for page loading
+    try:
+        WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.CLASS_NAME, 'ta_btn_cancel')))
+    except TimeoutException:
+        print("Timed out waiting for page to load")
+
+    # link_items = driver.find_elements(By.CLASS_NAME, 'ta-link-name')
+    # for link_item in link_items:
+    #     if link_item.text == '我要打卡':
+    #         print(link_item.text)
+    #         link_item.click()
+
+    link_items = driver.find_elements(By.CLASS_NAME, 'ta_btn_cancel')
+    for link_item in link_items:
+        if link_item.text != '休息開始':
+            print("你已經 %s 了" % link_item.text)
+            link_item.click()
+
 # login page
 Login()
+# want to clock in/out
+WantClockInOut1stLevel()
+WantClockInOut2ndLevel()
 
 driver.save_screenshot('screenshot.png')
